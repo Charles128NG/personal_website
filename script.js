cd4d988b8fd9ac183eeb10795f9bed3bc252fc87
Aryan <negiaryan53@gmail.com>
2021-11-23 00:51:33 +0530

    renamed main.html to index.html

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 	$(window).scroll(function(){
-		
+		/*For navbar*/
 		if(this.scrollY > 20){
 			$('.navbar').addClass("sticky");
 		}
@@ -9,9 +9,32 @@ $(document).ready(function(){
 			$('.navbar').removeClass("sticky");
 		}
 	});
-
+		/*For Hamburger menu*/
 	$('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+
+	/*For carousel*/
+	$('.carousel').owlCarousel({
+		margin:20,
+		loop:true,
+		autoplay: true,
+		autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+	});
 });
